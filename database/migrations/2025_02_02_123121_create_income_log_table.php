5f84471fdd08855f8dde1d007daa631ac7148261
KzAshimo <good.ashimo@gmail.com>
2025-02-02 22:03:48 +0900

    テーブル作成

---
+++ database/migrations/2025_02_02_123121_create_income_log_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('income_log', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('price');
+            $table->bigInteger('category_id')->unsigned()->index();
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->timestamps();
+            //外部キー結合
+            $table->foreign('category_id')->references('id')->on('income_category')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('income_log');
+    }
+};
