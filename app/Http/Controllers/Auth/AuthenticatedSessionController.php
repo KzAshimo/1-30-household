65b0c9761c790f44655d96ebb4728e9853f0615a
KzAshimo <good.ashimo@gmail.com>
2025-02-03 23:39:20 +0900

    adminrouteまで

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -30,9 +30,14 @@ class AuthenticatedSessionController extends Controller
     public function store(LoginRequest $request): RedirectResponse
     {
         $request->authenticate();
-
         $request->session()->regenerate();
 
+        //roleがadminならadmin.dashboardのrouteへ
+        if (auth()->user()->role === 'admin') {
+            return redirect()->intended(route('admin.dashboard', absolute: false));
+        }
+
+        //roleがadmin以外ならdashboardのrouteへ
         return redirect()->intended(route('dashboard', absolute: false));
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +23,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+//admin route
+Route::middleware(['auth',AdminMiddleware::class])->group(function(){
+    Route::get('/admin/dashboard',function(){
+        return Inertia::render('AdminDashboard');
+    })->name('admin.dashboard');
 });
 
 require __DIR__.'/auth.php';
